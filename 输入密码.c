acb7d1154b98db5029ca7094444ff88a791c6d81
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-08-24 10:38:59 +0800

    Add files via upload

--- 输入密码.c
+++ 输入密码.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(){
-    int ch;
+    char ch;
     int password;
     printf("请输入密码:\n");
     scanf("%d",&password);
-    printf("确认密码?\n    1确认,2取消确认\n    (1/2)\n");
-    scanf("%d",&ch);
-if(ch == 1)
+    printf("确认密码?\n(Y/N)\n");
+    scanf("%s",&ch);
+if(ch == 'Y')
     {printf("确认密码\n");
     if(password == 1234)
         {printf("密码正确\n");}
