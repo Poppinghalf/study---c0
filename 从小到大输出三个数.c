8918ff34656a0d70175450f6ceae6633d205db6f
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-08-26 17:42:00 +0800

    Add files via upload

---
+++ 从小到大输出三个数.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("请输入三个数字\n");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b)
+    {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    if(a>c)
+    {
+        int tmp = a;
+        a = c;
+        c = tmp;
+    }
+    if(b>c)
+    {
+        int tmp = b;
+        b = c;
+        c = tmp;
+    }
+    printf("%d %d %d",a,b,c);
+    return 0;
+}
