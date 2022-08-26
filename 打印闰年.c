3af491822710f0a7a8e3805851b598bfdceda453
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-08-26 18:32:41 +0800

    Add files via upload

--- 打印闰年.c
+++ 打印闰年.c
@@ -2,11 +2,16 @@
 int main()
 {
     int year;
-    for(year = 1000;year<=5000;year++)
+    for(year = 1000;year<=2000;year++)
     {
         if(year%4 ==0&&year%100 != 0)
         {
             printf("%d  ",year);
+        }
+        else if(year%400 ==0)
+        {                       //闰年判断标准：
+            printf("%d  ",year);//1.能被4整除不能被100整除
+                                //2.能被400整除
         }
     }
     return 0;
