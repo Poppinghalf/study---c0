cb730d26d3bd65dd813e51efcbc0d021fc360d6d
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-08-26 21:35:51 +0800

    Add files via upload

--- 打印闰年.c
+++ 打印闰年.c
@@ -19,3 +19,19 @@ int main()
     printf("\n一共有%d个年份\n",count);
     return 0;
 }
+// #include<stdio.h>
+// int main()
+// {
+//     int year;
+//     int count = 0;
+//     for(year = 1000;year<=2000;year++)
+//     {
+//         if(year % 4 == 0 && year%100 != 0 ||year % 400 == 0)//使用逻辑或||
+//         {
+//             printf("%d  ",year);
+//             count++;
+//         }
+//     }
+//     printf("\n总的年份个数是:%d",count);
+//     return 0;
+// }
