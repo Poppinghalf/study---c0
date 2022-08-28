9b16488bd6a028692ed258502b34a313baf89165
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-08-28 19:18:49 +0800

    Add files via upload

--- struct与指针变量.c
+++ struct与指针变量.c
@@ -10,9 +10,21 @@ int main(){
     struct people a1 = {"王德发",18,180,666666};
     struct people* pb = &a1;
     printf("名字:%s\n",pb->name);
-    printf("年龄：%d岁\n",pb->age);
+    strcpy(pb->name,"王尼玛");
+    printf("改名:%s\n",pb->name);
+    printf("年龄:%d岁\n",pb->age);
     printf("身高%dcm\n",pb->height);
-    printf("学号：%d\n",pb->codenumber);
+    printf("学号:%d\n",pb->codenumber);
     return 0;
-
 }
+// int main()
+// {
+//     struct people a1 = {"王德发",18,180,666666};
+//     printf("名字:%s\n",a1.name);
+//     strcpy(a1.name,"王尼玛");
+//     printf("改名:%s\n",a1.name);
+//     printf("年龄:%d\n",a1.age);
+//     printf("身高:%d\n",a1.height);
+//     printf("学号:%d\n",a1.codenumber);
+//     return 0;
+// }
