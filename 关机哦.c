656c27c95074ff9249868bf1c71ed1aadea471a1
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-08-28 18:03:12 +0800

    Add files via upload

--- 关机哦.c
+++ 关机哦.c
@@ -5,11 +5,18 @@ int main()
 {
     char input[20];
     system("shutdown -s -t 60");
-    printf("再过一分钟电脑就关机了哦，输入“手滑了”取消关机\n");
+    printf("再过一分钟电脑就关机了哦，输入“手滑了”取消关机\n请输入\n");
+    while(1)
+    {
     scanf("%s",&input);
     if((strcmp(input,"手滑了") == 0))
     {
         system("shutdown -a");
+        printf("现在没事了\n");
+        break;
+    }
+    else
+    printf("还想不想阻止关机了\n");
     }
     return 0;
 }
