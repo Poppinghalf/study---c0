ef082dc96a9521d90008642a62feb5e2712fbfc8
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-08-29 23:40:40 +0800

    Add files via upload

---
+++ 函数（在有序数组中找到数）.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+
+char arr[] = {1,2,3,4,5,6,7,8,9,10};
+
+void search(int x)
+{
+    int left  =  0;
+    int sz = sizeof(arr)/sizeof(arr[0]);
+    int right = sz-1;
+
+while(left<=right)
+{   
+    int mid = (left+right)/2;
+    if(arr[mid] < x)
+    {
+        left = mid+1;
+    }
+    else if(arr[mid] > x)
+    {
+        right = mid-1;
+    }
+    else
+    {
+        printf("找到了,下标是%d\n",mid);
+        break;
+    }
+}
+if(left>right)
+{
+    printf("找不到\n");
+}
+}
+
+
+int main()
+{
+    int n;
+    printf("请输入1-10的数字\n");
+    scanf("%d",&n);
+    search(n);
+    return 0;
+}
