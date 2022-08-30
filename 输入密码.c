8a93db4d57b1edd447db09e0a7ae432100a6e1eb
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-08-30 15:54:14 +0800

    Add files via upload

--- 输入密码.c
+++ 输入密码.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdio.h>
 int main(){
     char ch;
     int password;
@@ -18,3 +19,31 @@ else
     {printf("取消确认\n");}
     return 0;
 }
+
+
+// #include<stdio.h>
+// #include<string.h>
+// int main()
+// {
+//     char ch[1];
+//     char password[20];
+//     printf("请输入密码\n");
+//     scanf("%s",&password);
+//     printf("确认密码?\n(Y/N)\n");
+//     scanf("%s",ch);
+//     if(strcmp(ch,"Y")==0)
+//     {
+//         if(strcmp(password,"abc12345")==0)
+//         {
+//             printf("密码正确");
+//         }
+//         else
+//         {
+//             printf("密码错误");
+//         }
+//     }
+//     else
+//     {
+//         printf("取消确认");
+//     }
+// }
