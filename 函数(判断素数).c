ce4cfba64a97a10ffe68800f847fc3064f2d6299
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-08-30 12:54:59 +0800

    Add files via upload

--- 函数(判断素数).c
+++ 函数(判断素数).c
@@ -1,27 +1,62 @@
+// #include<stdio.h>
+
+
+// void fuck(int x)
+// {
+//     int i;
+//     for(i=2;i<x;i++)
+//     {
+//     if(x%i==0)
+//     {
+//         printf("这个数不是素数\n");
+//         break;
+//     }
+//     }
+//     if(i == x)
+//     {
+//         printf("这个数是素数");
+//     }
+// }
+
+
+// int main()
+// {
+//     int n;
+//     printf("请输入数字\n");
+//     scanf("%d",&n);
+//     fuck(n);
+//     return 0;
+// }
 #include<stdio.h>
 
 
-void fuck(int x)
+int fuck(int x)
 {
-    int i;
-    for(i=2;i<x;i++)
-    if(x%i==0)
+    int j;
+    for(j=2;j<x;j++)
     {
-        printf("这个数不是素数\n");
-        break;
+        if(x%j==0)
+        {
+            return 0;
+            break;
+        }
     }
-    if(i == x)
+    if(j==x)
     {
-        printf("这个数是素数");
+        return 1;
     }
 }
 
 
 int main()
 {
-    int n;
-    printf("请输入数字\n");
-    scanf("%d",&n);
-    fuck(n);
+    int i;
+    for(i = 100;i<=200;i++)
+    {
+        if(fuck(i)==1)
+        {
+            printf("%d   ",i);
+        }
+    }
     return 0;
 }
--- 函数(判断闰年).c
+++ 函数(判断闰年).c
@@ -22,3 +22,28 @@ int main()
     fuck(year);
     return 0;
 }
+// #include<stdio.h>
+
+// int fuck(int x)
+// {
+//     if(((x%4==0)&&(x%100!=0))||(x%400==0))
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
+// int main()
+// {
+//     int year;
+//     for(year=1000 ; year<=2000 ; year++)
+//     {
+//         if(fuck(year)==1)
+//         {
+//             printf("%d  ",year);
+//         }
+//     }
+//     return 0;
+// }
