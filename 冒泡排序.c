b9b85de5d7a9a2b0190e844497715ce46e6c6584
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-09-03 21:07:13 +0800

    Add files via upload

--- 冒泡排序.c
+++ 冒泡排序.c
@@ -5,6 +5,7 @@ void fuck(int* arr,int sz)
     int i;
     for(i = 0;i<sz-1;i++)//确认趟数
     {
+        int flag = 1;
         int j;
         for(j=0;j<sz-1-i;j++)
         {
@@ -13,7 +14,12 @@ void fuck(int* arr,int sz)
                 int tmp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = tmp;
+                flag = 0;//本趟不完全排序
             }
+        }
+        if(flag == 1)
+        {
+            break;
         }
     }
 }
@@ -21,7 +27,7 @@ void fuck(int* arr,int sz)
 
 int main()
 {
-    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    int arr[10]={6,9,66,4,88,68,7,52,9,10};
     int i;
     int sz = sizeof(arr)/sizeof(arr[0]);
     fuck(arr,sz);
