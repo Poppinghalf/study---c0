51dd9d3f6c97024777b73a4aabc19d0da53b08a0
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-09-07 11:36:41 +0800

    Add files via upload

--- 输入密码.c
+++ 输入密码.c
@@ -1,22 +1,28 @@
 #include<stdio.h>
-#include<stdio.h>
+#include<string.h>
 int main(){
     char ch;
-    int password;
+    char password[20];
     printf("请输入密码:\n");
-    scanf("%d",&password);
+    scanf("%s",password);
     getchar();//带走输入缓冲区的"\n""
     printf("确认密码?\n(Y/N)\n");
     ch = getchar();
 if(ch == 'Y')
-    {printf("确认密码\n");
-    if(password == 1234)
-        {printf("密码正确\n");}
-
+    {
+    if(strcmp(password,"abc123456") == 0)
+    {
+        printf("密码正确\n");
+    }
     else
-        printf("密码错误\n");}
+    {
+    printf("密码错误\n");
+    }
+    }
 else
-    {printf("取消确认\n");}
+    {
+    printf("取消确认\n");
+    }
     return 0;
 }
 
@@ -28,7 +34,7 @@ else
 //     char ch[1];
 //     char password[20];
 //     printf("请输入密码\n");
-//     scanf("%s",&password);
+//     scanf("%s",password);
 //     printf("确认密码?\n(Y/N)\n");
 //     scanf("%s",ch);
 //     if(strcmp(ch,"Y")==0)
--- 输入密码2.c
+++ 输入密码2.c
@@ -5,8 +5,9 @@ int main()
     char password[20];
     int i;
     printf("请输入密码\n");
-    for(i=1;i<=5;i++)
+    for(i=1;i<6;i++)
     {
+        int c = 5-i;
         scanf("%s",password);
         if(strcmp(password,"123456") == 0)
             {
@@ -15,9 +16,11 @@ int main()
             }
         else
             {
-            int c = 5-i;
-            printf("密码错误,你还有%d次机会\n",c);
-            }4
+            if(c>0)
+            {
+                printf("密码错误,你还有%d次机会\n",c);
+            }
+            }
     }
     if(i==6)
     printf("密码错误五次,账户锁定\n");
