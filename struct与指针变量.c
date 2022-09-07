eded427535a5448ee9bda721f281a6e536e60f64
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-09-07 10:45:02 +0800

    Add files via upload

--- struct与指针变量.c
+++ struct与指针变量.c
@@ -1,30 +1,32 @@
 #include<stdio.h>
 #include<string.h>
-struct  people{
+struct  people
+{
     char name[20];//名字
     short age;//年龄
     int height;//身高
-    long long codenumber;//学号
+    char codenumber[20];//学号
 };
-int main(){
-    struct people a1 = {"王德发",18,180,666666};
+int main()
+{
+    struct people a1 = {"王德发",18,180,"abcabc12345"};
     struct people* pb = &a1;
     printf("名字:%s\n",pb->name);
     strcpy(pb->name,"王尼玛");
     printf("改名:%s\n",pb->name);
     printf("年龄:%d岁\n",pb->age);
     printf("身高%dcm\n",pb->height);
-    printf("学号:%d\n",pb->codenumber);
+    printf("学号:%s\n",pb->codenumber);
     return 0;
 }
 // int main()
 // {
-//     struct people a1 = {"王德发",18,180,666666};
+//     struct people a1 = {"王德发",18,180,"abcabc12345"};
 //     printf("名字:%s\n",a1.name);
 //     strcpy(a1.name,"王尼玛");
 //     printf("改名:%s\n",a1.name);
 //     printf("年龄:%d\n",a1.age);
 //     printf("身高:%d\n",a1.height);
-//     printf("学号:%d\n",a1.codenumber);
+//     printf("学号:%s\n",a1.codenumber);
 //     return 0;
 // }
