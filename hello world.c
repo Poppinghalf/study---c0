e349965baec949546a97ec80cffd987599fd1e7c
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-09-13 23:24:25 +0800

    Add files via upload

--- hello world.c
+++ hello world.c
@@ -6,7 +6,8 @@ int main()
 char arr1[]={"hello world!!!"};
 char arr2[]={"              "};
 int left = 0;
-while(left<strlen(arr2))
+int end = strlen(arr2)-1;
+while(left<end)
 {
     arr2[left] = arr1[left];
     left++;
---
+++ 作业：字符串逆序.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char n;
+    char arr[] = {"abcdef"};
+    int left = 0;
+    int right = strlen(arr)-1;
+    printf("%s\n",arr);
+    while(left<right)
+    {
+        n = arr[left];
+        arr[left] = arr[right];
+        arr[right] = n;
+        left++;
+        right--;
+    }
+    printf("%s\n",arr);
+    return 0;
+}
+
+
+// //ตÝน้
+// #include<stdio.h>
+
+
+// int my_strlen(char* arr)
+// {
+//     int count = 0;
+//     while(*arr != '\0')
+//     {
+//         count++;
+//         arr++;
+//     }
+//     return count;
+// }
+
+
+// void nixu(char arr[])
+// {
+//     char tmp = arr[0];
+//     int len = my_strlen(arr);
+//     arr[0] = arr[len-1];
+//     arr[len-1] = '\0';
+//     if(my_strlen(arr+1)>=2)
+//     {
+//         nixu(arr+1);
+//     }
+//     arr[len-1] = tmp;
+// }
+
+
+// int main()
+// {
+//     char arr[] = {"abcdef"};
+//     printf("%s\n",arr);
+//     nixu(arr);
+//     printf("%s\n",arr);
+//     return 0;
+// }
