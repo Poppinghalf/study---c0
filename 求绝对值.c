9416ca06a5731e207635983e64da2f66e870f8e3
Poppinghalf <111716304+Poppinghalf@users.noreply.github.com>
2022-09-17 21:30:02 +0800

    Add files via upload

--- 求绝对值.c
+++ 求绝对值.c
@@ -1,23 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    double a;
-    int i;
-    printf("要输入几组\n");
-    scanf("%d",&i);
-    for(;i>0;i--)
+    double i;
+    while(~scanf("%lf",&i))
     {
-        printf("输入数字\n");
-        scanf("%lf",&a);
-        if(a<0)
+        if(i>=0)
         {
-            a = -a;
+            printf("%.2lf\n",i);
         }
         else
         {
-            a = a;
+            printf("%.2lf\n",-i);
         }
-        printf("%.2lf\n",a);
     }
     return 0;
 }
